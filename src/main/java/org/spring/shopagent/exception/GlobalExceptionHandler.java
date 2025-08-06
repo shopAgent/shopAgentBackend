fcd5b3f835afb710dbf9f93c39dd2061de88f06b
Huni <hunilee@carroglobal.com>
2025-08-07 00:17:15 +0900

    fix: Resolve GraalVM error in GitHub Actions workflow
    - Change Gradle graalvmNative Option

--- src/main/java/org/spring/shopagent/exception/GlobalExceptionHandler.java
+++ src/main/java/org/spring/shopagent/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package org.spring.shopagent.exception;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.spring.shopagent.response.ApiResponseDto;
 import org.spring.shopagent.response.ResponseUtils;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +8,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class )
     public ResponseEntity<ApiResponseDto<Void>> methodValidException(MethodArgumentNotValidException e) {
         ErrorResponse responseDto = ErrorResponse.of(e.getBindingResult());
-        log.error("methodValidException throw Exception : {}", e.getBindingResult());
 
         return ResponseEntity.badRequest().body(ResponseUtils.error(responseDto));
     }
@@ -24,7 +21,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = CustomException.class)
     protected ResponseEntity<ApiResponseDto<Void>> handleCustomException(CustomException e) {
         ErrorResponse responseDto;
-        log.error("handleDataException throw Exception : {}", e.getErrorType());
         if (e.getCustomMessage() != null && !e.getCustomMessage().isEmpty())
             responseDto = ErrorResponse.of(e.getErrorType(), e.getCustomMessage());
         else
